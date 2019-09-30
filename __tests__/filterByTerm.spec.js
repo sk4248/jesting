54e4a48173ab4dc40a7675654b2a64429e000485
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.home>
2019-09-30 17:58:07 -0400

    lets have some fun with automated testing

---
+++ __tests__/filterByTerm.spec.js
@@ -0,0 +1,19 @@
+
+const filterByTerm = require("../src/filterByTerm");
+
+describe("Filter Function", () => {
+  //test stuff
+  test("it should filter by a search term (link)", () => {
+  // actual test
+  const input = [
+    {id: 1, url: "https://www.url1.com"},
+    {id: 2, url: "https://www.url2.com"},
+    {id: 3, url: "https://www.link3.com"}
+  ];
+
+  const output = [{id: 3, url: "https://www.link3.com"}];
+
+  expect(filterByTerm(input,"link")).toEqual(output);
+  });
+
+})
